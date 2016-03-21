9842101556d3b3dadf0a7ca6ec61e4605543f862
Woo Yun <hh747hh@gmail.com>
2016-03-21 12:54:54 -0400

    oils controller, index view added

--- app/controllers/olis_controller.rb
+++ app/controllers/olis_controller.rb
@@ -1,6 +1,26 @@
 class OilsController < ApplicationController
 
-def index
+  def index
+    @oils = Oil.all
+    render :index
+
+  end
 
 
-END
+  def create
+    @oil  = Oil.create!(oil_params)
+    redirect_to "/oils/#{{@oil.id}}"
+  end
+
+
+  private
+  def oil_params
+    params.require(:oil).permit(:name, :photo_url, :benefits)
+  end
+
+
+
+
+
+
+end
