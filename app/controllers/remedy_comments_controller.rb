97d35e5a44218e7535b929b7c29dd3faa99190ad
Woo Yun <hh747hh@gmail.com>
2016-03-23 14:40:39 -0400

    working on comment delete function

--- app/controllers/remedy_comments_controller.rb
+++ app/controllers/remedy_comments_controller.rb
@@ -1,16 +1,47 @@
 class RemedyCommentsController < ApplicationController
 
   def create
-      @remedy = Remedy.find(params[:remedy_id])
-      @remedy_comment = @remedy.remedy_comments.create(comment_params)
-      redirect_to remedy_path(@remedy)
+    @remedy = Remedy.find(params[:remedy_id])
+    @remedy_comment = @remedy.remedy_comments.create(comment_params)
+    redirect_to remedy_path(@remedy)
+  end
+
+  def show
+    @remedy = Remedy.find(params[:remedy_id])
+    @remedy_comment = RemedyComment.find(params[:id])
+  end
+
+
+  def new
+    @remedy = Remedy.find(params[:remedy_id])
+    @remedy_comment = RemedyComment.new
+  end
+
+
+  def edit
+    @remedy = Remedy.find(params[:remedy_id])
+    @remedy_comment = RemedyComment.find(params[:id])
+  end
+
+  def update
+    @remedy = Remedy.find(params[:remedy_id])
+    @remedy_comment = RemedyComment.find(params[:id])
+    @remedy_comment.update(comment_params)
+    redirect_to remedy_path(@remedy)
+  end
+
+  def destroy
+    @remedy = Remedy.find(params[:remedy_id])
+    @remedy_comment = RemedyComment.find(params[:id])
+    @remedy_comment.destroy
+    redirect_to remedy_path(@remedy)
+
   end
 
   private
-    def comment_params
-      params.require(:remedy_comment).permit(:name, :comment)
-    end
-
+  def comment_params
+    params.require(:remedy_comment).permit(:name, :comment)
+  end
 
 
 
